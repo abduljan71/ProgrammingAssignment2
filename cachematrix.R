d0af4223db5f49ea93478a2f35ec4aae0c9fa09c
Abduljan71 <Abduljan71@gmail.com>
2020-07-22 18:10:16 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My function would be able to create a special kind of matrix object. In which, 
+## it can cache its inverse. Then, it will be able to compute the inverse of my first function. ##Put comments here that give an overall description of what your
 
-## Write a short comment describing this function
+## This would create a special kind of matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  solve(x)
 
 }
 
 
-## Write a short comment describing this function
+## This function would calculate the inverse of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
+  x <- solve(x)
+  answer <- x
+  want <- solve(answer)
+ 
+  return(want)
+  
         ## Return a matrix that is the inverse of 'x'
 }
